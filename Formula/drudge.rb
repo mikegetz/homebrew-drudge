272a286f2dcd4b18af9ab895c85ace8242d822f2
mikegetz <getzmd02@gmail.com>
2025-02-17 01:34:53 -0700

    release v1.2.5

--- Formula/drudge.rb
+++ Formula/drudge.rb
@@ -1,32 +1,32 @@
 class Drudge < Formula
   desc "A CLI that prints the drudge page headlines to stdout"
   homepage "https://github.com/mikegetz/drudge"
-  version "1.2.4"
+  version "1.2.5"
   license "MIT"  # Adjust according to your license
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/mikegetz/drudge/releases/download/v1.2.4/drudge-v1.2.4-windows-amd64"
-      sha256 "220a16be854d42e1d3a38cda2cd05130086e4bfa1d284f62476c30b7b7ad81fe"
+      url "https://github.com/mikegetz/drudge/releases/download/v1.2.5/drudge-v1.2.5-windows-amd64"
+      sha256 "604d7d9dd5731b4e5452407975a001ad4c0510b26a6c00fa5ff8061ca1598f2b"
     elsif Hardware::CPU.arm?
-      url "https://github.com/mikegetz/drudge/releases/download/v1.2.4/drudge-v1.2.4-darwin-arm64"
-    sha256 "fa46a8e74eb8f669bfaa982d28aa5709624eb960ed9c865cacc6a775289e0ec5"
+      url "https://github.com/mikegetz/drudge/releases/download/v1.2.5/drudge-v1.2.5-darwin-arm64"
+    sha256 "abcfae231a950c32b91033a3523c627c9c69b1a49cf14aff6dfd5290cbc752d2"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/mikegetz/drudge/releases/download/v1.2.4/drudge-v1.2.4-darwin-amd64"
-      sha256 "54bb6fdf71b1fce8f1175b8ec3a2849de28819d1187a2e0fefc09c14b8005a01"
+      url "https://github.com/mikegetz/drudge/releases/download/v1.2.5/drudge-v1.2.5-darwin-amd64"
+      sha256 "a4b14f4c96a7ecf8f3b32df61caefadbd44df107a1399ba61d0cbbb9dd969932"
     elsif Hardware::CPU.arm?
-      url "https://github.com/mikegetz/drudge/releases/download/v1.2.4/drudge-v1.2.4-linux-arm64"
-      sha256 "cd6fc9d26815171428f08844eb82348b9167d1f2607e7098a11272d0fa47c5d8"
+      url "https://github.com/mikegetz/drudge/releases/download/v1.2.5/drudge-v1.2.5-linux-arm64"
+      sha256 "b84bd062036be7b8a6d470ee28de1ba007d5b8a32e64b783cd3e1ffa80b351d8"
     end
   end
 
   def install
-    chmod "+x", "drudge-v1.2.4-darwin-arm64"
-  bin.install "drudge-v1.2.4-darwin-arm64" => "drudge"
+    chmod "+x", "drudge-v1.2.5-darwin-arm64"
+  bin.install "drudge-v1.2.5-darwin-arm64" => "drudge"
   end
 
   test do
